33d05fdb98b2dc62d0e460867ca1d24427fb1d38
Ragib-hasin <ragibhasin295@gmail.com>
2026-05-14 23:18:05 +0600

    Fixed comment Controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,12 +1,9 @@
 import Comment from "../models/comment.model.js";
-
 const commentSection = async (req, res) => {
   try {
-    const { blogID, name, email, content } = req.body;
+    let { blogID, name, email, content } = req.body;
 
-    blogID = new ObjectId(blogID);
-
-    const result = await Comment.create(blogID, name, email, content);
+    let result = await Comment.create({ blogID, name, email, content });
 
     res.status(201).json({
       success: true,
